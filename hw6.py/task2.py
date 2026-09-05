4c8ef85e9de3d9c96cd885f794a10272903af028
Wasyyy <maticwew@gmail.com>
2026-09-05 19:26:55 +0300

    Delete hw6.py directory

--- hw6.py/task2.py
+++
@@ -1,4 +0,0 @@
-length = int(input('Длина прямоугольника:'))
-width = int(input('Ширина прямоугольника:'))
-scace = length*width
-print(f'Площадь прямоугольника: {scace}')
--- hw6.py/task3.py
+++
@@ -1,3 +0,0 @@
-celsius = int(input('Введите температуру в градусах цельсия:'))
-fahrenheit = celsius * 9 / 5 + 32
-print(f'{celsius}C это {fahrenheit}F')
